6c377767a8342b87ee01fe343bc0b28e56f85fa2
Rapeephat-kp <rapeephat.k@kkumail.com>
2025-08-20 14:23:55 +0700

    Update6

--- Script/Test_map.gd
+++ Script/Test_map.gd
@@ -4,10 +4,10 @@ func _ready() -> void:
 	print(Gamemanager.get_p())
 	var p_value = Gamemanager.get_p()
 	if p_value == 1:
-		$CanvasUI/Elendros.visible = true
-		$CanvasUI/Nymera.visible = false
+		$CanvasUI/Player_show/Elendros.visible = true
+		$CanvasUI/Player_show/Nymera.visible = false
 	elif p_value == 2:
-		$CanvasUI/Elendros.visible = false
-		$CanvasUI/Nymera.visible = true
+		$CanvasUI/Player_show/Elendros.visible = false
+		$CanvasUI/Player_show/Nymera.visible = true
 func _process(delta: float):
-	$CanvasUI/Inventory.initialize_inventory()
+	$CanvasUI/Inventory/Inventory.initialize_inventory()
