5063b64125a4ce1ee78d24e3147e6613265f0151
Rapeephat-kp <rapeephat.k@kkumail.com>
2025-08-20 13:57:01 +0700

    Update4

--- Scene/setting.gd
+++ Scene/setting.gd
@@ -1,4 +1,16 @@
 extends Node2D	
 
-func _on_button_pressed() -> void:
-	print("jaja")
+
+
+
+func _on_setting_button_pressed() -> void:
+	$"../AnimationPlayer".play("Open")
+
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scene/choose_player.tscn")
+
+
+func _on_resume_button_pressed() -> void:
+	$"../AnimationPlayer".play("Close")
