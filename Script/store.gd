29684b7ebc38d3c5f38ac191d5032cdee15c6822
Rapeephat-kp <rapeephat.k@kkumail.com>
2025-08-21 18:33:55 +0700

    Update11
    Fully function Store without text

--- Script/store.gd
+++ Script/store.gd
@@ -6,20 +6,25 @@ func _on_hp_potion_pressed() -> void:
 	var cam = $Control/HP_Potion/Camera2D
 	click_check = !click_check   
 	cam.enabled = click_check
-	$Control/Default.visible = !click_check
-	$Control/Blur.visible = click_check
-	$Control/HP_Potion/Panel.visible = click_check
+	$CanvasLayer.visible = !click_check
+	
 	
 func _on_max_hp_potion_pressed() -> void:
 	var cam = $Control/Max_HP_Potion/Camera2D
 	click_check = !click_check  
 	cam.enabled = click_check
+	$CanvasLayer.visible = !click_check
+	
 
 func _on_hp_buy_pressed() -> void:
 	if Gamemanager.get_coin() >= 100:
 		PlayerInventory.add_item("Healing Potion",1)
 		Gamemanager.set_coin(-100)
 		
-
+func _on_max_hp_buy_pressed() -> void:
+	if Gamemanager.get_coin() >= 350:
+		PlayerInventory.add_item("Increase Max Hp Potion",1)
+		Gamemanager.set_coin(-350)
+		
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scene/Map.tscn")
